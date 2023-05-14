e91b99818ea13bdda777cce0344405b8190392c5
Sagnik Roy Chaudhury <108208974+Sagnik02@users.noreply.github.com>
2023-05-14 16:00:56 +0530

    Add files via upload

---
+++ Basics_2/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html',name="Sagnik")
+
+
+app.run(host='0.0.0.0', port=8080)
